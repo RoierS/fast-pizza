021584c25114cf41c6918cfe14084186f4e31d5e
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-01-12 18:33:03 +0100

    feat: implement routing within app layout

---
+++ src/ui/AppLayout.tsx
@@ -0,0 +1,21 @@
+import { Outlet } from 'react-router-dom';
+
+import CartOverview from '@/features/cart/CartOverview';
+
+import Header from './Header';
+
+const AppLayout: React.FC = () => {
+  return (
+    <div>
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <CartOverview />
+    </div>
+  );
+};
+
+export default AppLayout;
---
+++ src/ui/Header.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <Link to="/">Fast Pizza Co.</Link>
+
+      <p>Test</p>
+    </header>
+  );
+};
+
+export default Header;
