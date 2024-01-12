36b042d936aa3cb1c14a138e9f23c07b21e709fc
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-01-12 13:23:27 +0100

    feat: implement apiRestaurant

---
+++ src/services/apiRestaurant.ts
@@ -0,0 +1,56 @@
+const API_URL = 'https://react-fast-pizza-api.onrender.com/api';
+
+export const getMenu = async () => {
+  const res = await fetch(`${API_URL}/menu`);
+
+  if (!res.ok) throw new Error('failed getting menu');
+
+  const { data } = await res.json();
+  return data;
+};
+
+export const getOrder = async (id: string) => {
+  const res = await fetch(`${API_URL}/order/${id}`);
+
+  if (!res.ok) throw new Error(`Couldn't find order #${id}`);
+
+  const { data } = await res.json();
+  return data;
+};
+
+interface IOrder {}
+
+export const createOrder = async (newOrder: IOrder) => {
+  try {
+    const res = await fetch(`${API_URL}/order/${newOrder}`, {
+      method: 'POST',
+      body: JSON.stringify(newOrder),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!res.ok) throw Error();
+
+    const { data } = await res.json();
+    return data;
+  } catch (error) {
+    throw Error('Could not create an order');
+  }
+};
+
+export const updateOrder = async (id: string, updatedOrder: IOrder) => {
+  try {
+    const res = await fetch(`${API_URL}/order/${id}`, {
+      method: 'PATCH',
+      body: JSON.stringify(updatedOrder),
+      headers: {
+        'Content-Type': 'Application/json',
+      },
+    });
+
+    if (!res.ok) throw Error;
+  } catch (error) {
+    throw Error('Error updating order');
+  }
+};
