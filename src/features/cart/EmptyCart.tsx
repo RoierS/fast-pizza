50be24266630ccb9f1859508ac83f2beb4aa19ce
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-01-16 18:03:22 +0100

    style: style empty cart component

--- src/features/cart/EmptyCart.tsx
+++ src/features/cart/EmptyCart.tsx
@@ -1,11 +1,13 @@
-import { Link } from 'react-router-dom';
+import LinkButton from '@/ui/LinkButton';
 
 const EmptyCart: React.FC = () => {
   return (
-    <div>
-      <Link to="/menu">&larr; Back to menu</Link>
+    <div className="mx-auto space-y-8 px-4 py-6">
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <p>Your cart is still empty. Start adding some pizzas :)</p>
+      <p className="text-center font-semibold">
+        Your cart is still empty 🛒. Start adding some pizzas 🍕🍕
+      </p>
     </div>
   );
 };
